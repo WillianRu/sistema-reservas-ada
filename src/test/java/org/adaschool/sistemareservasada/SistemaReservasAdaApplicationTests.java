5c1e31ca7f762b91554d5eaabba012d6c7fc81aa
WillianRu <correo.willianrueda@gmail.com>
2023-12-04 18:55:32 -0500

    Inicializacion springboot

---
+++ src/test/java/org/adaschool/sistemareservasada/SistemaReservasAdaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adaschool.sistemareservasada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaReservasAdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
