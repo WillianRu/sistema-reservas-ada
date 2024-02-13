75d7df133a671cee8ebf07769e6439726c2eb4fd
Willian <correo.willianrueda@gmail.com>
2024-02-12 20:01:38 -0500

    Cors implementation

---
+++ src/main/java/org/adaschool/sistemareservasada/application/config/cors/CorsGlobalConfig.java
@@ -0,0 +1,27 @@
+package org.adaschool.sistemareservasada.application.config.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsGlobalConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+
+        config.addAllowedOrigin("http://localhost:4200");
+
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
